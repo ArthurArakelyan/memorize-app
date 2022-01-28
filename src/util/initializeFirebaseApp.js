06a07fe9222470906a4e95ece221b01d3f503128
ArturArakelyan0411 <artur.araqelyan.2004@gmail.com>
2022-01-28 13:58:17 +0400

    changed initializeApp options

--- src/util/initializeFirebaseApp.js
+++ src/util/initializeFirebaseApp.js
@@ -4,12 +4,12 @@ const REACT_APP = 'REACT_APP_';
 
 const initializeFirebaseApp = () => {
   initializeApp({
-    apiKey: process.env[`${REACT_APP}API_KEY`],
-    authDomain: process.env[`${REACT_APP}AUTH_DOMAIN`],
-    projectId: process.env[`${REACT_APP}PROJECT_ID`],
-    storageBucket: process.env[`${REACT_APP}STORAGE_BUCKET`],
-    messagingSenderId: process.env[`${REACT_APP}MESSAGING_SENDER_ID`],
-    appId: process.env[`${REACT_APP}APP_ID`]
+    apiKey: process.env[`${REACT_APP}API_KEY`] || process.env.API_KEY,
+    authDomain: process.env[`${REACT_APP}AUTH_DOMAIN`] || process.env.AUTH_DOMAIN,
+    projectId: process.env[`${REACT_APP}PROJECT_ID`] || process.env.PROJECT_ID,
+    storageBucket: process.env[`${REACT_APP}STORAGE_BUCKET`] || process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env[`${REACT_APP}MESSAGING_SENDER_ID`] || process.env.MESSAGING_SENDER_ID,
+    appId: process.env[`${REACT_APP}APP_ID`] || process.env.APP_ID,
   });
 }
 
