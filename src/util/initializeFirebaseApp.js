175d8079d90caac13f34ef6306891fdd67c748fd
ArturArakelyan0411 <artur.araqelyan.2004@gmail.com>
2022-01-30 14:50:56 +0400

    deleted .env console.log

--- src/util/initializeFirebaseApp.js
+++ src/util/initializeFirebaseApp.js
@@ -3,14 +3,13 @@ import {initializeApp} from "firebase/app";
 const REACT_APP = 'REACT_APP_';
 
 const initializeFirebaseApp = () => {
-  console.log(process.env)
   initializeApp({
-    apiKey: process.env[`${REACT_APP}API_KEY`] || process.env.API_KEY,
-    authDomain: process.env[`${REACT_APP}AUTH_DOMAIN`] || process.env.AUTH_DOMAIN,
-    projectId: process.env[`${REACT_APP}PROJECT_ID`] || process.env.PROJECT_ID,
-    storageBucket: process.env[`${REACT_APP}STORAGE_BUCKET`] || process.env.STORAGE_BUCKET,
-    messagingSenderId: process.env[`${REACT_APP}MESSAGING_SENDER_ID`] || process.env.MESSAGING_SENDER_ID,
-    appId: process.env[`${REACT_APP}APP_ID`] || process.env.APP_ID,
+    apiKey: process.env[`${REACT_APP}API_KEY`],
+    authDomain: process.env[`${REACT_APP}AUTH_DOMAIN`],
+    projectId: process.env[`${REACT_APP}PROJECT_ID`],
+    storageBucket: process.env[`${REACT_APP}STORAGE_BUCKET`],
+    messagingSenderId: process.env[`${REACT_APP}MESSAGING_SENDER_ID`],
+    appId: process.env[`${REACT_APP}APP_ID`],
   });
 }
 
